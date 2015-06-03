8de23935c1c6370f805377824744bb9b1800231f
Erik Weilow <nissehutt@gmail.com>
2015-06-03 21:47:21 +0200

    User browsing

--- routes/browse.js
+++ routes/browse.js
@@ -32,36 +32,40 @@ module.exports.router = function (app, offsiterepositories, localrepository) {
       func(files, 0, [], function (err, files) {
         if (err) return next(err);
         
-        res.render("browse.jade", { files: files, node: req.params.node, offsite: offsite});
+        res.render("browse.jade", { files: files, base: "/browse/node/" + req.params.node, node: req.params.node, offsite: offsite});
       });
     });
   });
   
-  router.get("/node/:node/:filename", function (req, res, next) {
+
+  var routeFile = function (req, res, next) {
     var repository = null;
     if (req.params.node === "local") {
       repository = localrepository;
-    } else if (offsiterepositories.hasOwnProperty(req.params.node))  {
+    } else if (offsiterepositories.hasOwnProperty(req.params.node)) {
       repository = offsiterepositories[req.params.node];
     }
-    
+
     if (path.extname(req.params.filename) === ".manifest") {
       repository.getFileAsJson(req.params.filename, function (err, file) {
         if (err) return next(err);
-        
+
         res.render("media", { name: req.params.node, base: "/browse/node/" + req.params.node, manifest: file });
       });
     } else return res.status(500).end();
-  });
+  };
   
-  router.get("/node/:node/raw/:filename", function (req, res, next) {
+  router.get("/node/:node/:filename", routeFile);
+  router.get("/user/:id/node/:node/:filename", routeFile);
+  
+  var routeRaw = function (req, res, next) {
     var repository = null;
     if (req.params.node === "local") {
       repository = localrepository;
-    } else if (offsiterepositories.hasOwnProperty(req.params.node))  {
+    } else if (offsiterepositories.hasOwnProperty(req.params.node)) {
       repository = offsiterepositories[req.params.node];
     }
-    
+
     if (path.extname(req.params.filename) !== ".manifest") {
       repository.getFile(req.params.filename, function (err, file) {
         if (err) return next(err);
@@ -69,6 +73,47 @@ module.exports.router = function (app, offsiterepositories, localrepository) {
         res.end();
       });
     } else return res.write(null).status(500).end();
+  };
+  router.get("/node/:node/raw/:filename", routeRaw);
+  router.get("/user/:id/node/:node/raw/:filename", routeRaw);
+  
+  
+  router.get("/user/:id/node/:node/", function (req, res, next) {
+    var repository = null;
+    var offsite = [{ href: "/browse/node/local", title: "repository" }];
+    if (req.params.node === "local") {
+      repository = localrepository;
+    } else if (offsiterepositories.hasOwnProperty(req.params.node))  {
+      repository = offsiterepositories[req.params.node];
+    }
+    repository.getFilesOfType([".manifest"], function (err, files) {
+      if (err) return next(err);
+      
+      var func = function (data, index, collect, cb) {
+        if (index >= data.length) return cb(null, collect);
+        
+        repository.getFileAsJson(data[index], function (err, file) {
+          if (err) return cb(err, null);
+          
+          file.manifestfile = data[index];
+          collect.push(file);
+          func(data, index + 1, collect, cb);
+        });
+      }
+      func(files, 0, [], function (err, files) {
+        if (err) return next(err);
+        
+        var r = [];
+        
+        files.forEach(function (file) {
+          if (file.author && file.author.id == req.params.id) {
+            r.push(file);
+          }
+        });
+        
+        res.render("browse.jade", { files: r, base: "/browse/node/" + req.params.node, node: "user " + r[0].author.username, offsite: offsite});
+      });
+    });
   });
   
   return router;
